b80b7e1227851ed1354ae6902a1cd103012d07b2
Jorteck <jorteckdev@gmail.com>
2021-05-05 11:17:38 +0000

    Update APIs (NWNX: 05aaf9c0, NWN: 8193.23).

--- src/NWNX/Plugins/EventsPlugin.cs
+++ src/NWNX/Plugins/EventsPlugin.cs
@@ -270,7 +270,7 @@ namespace NWN.Core.NWNX
     
     _______________________________________
         ## Has Feat Events
-        - NWNX_ON_HASFEAT_BEFORE
+        - NWNX_ON_HAS_FEAT_BEFORE
         - NWNX_ON_HAS_FEAT_AFTER
     
         `OBJECT_SELF` = The player being checked for the feat
