22462607c5a1e6a1bc17b390aca6c08baeaec725
Jhett Black <10942655+jhett12321@users.noreply.github.com>
2025-01-18 20:34:35 +0100

    Refactor NWNCore. Remove redundant code.

--- src/Core/NWNCore.cs
+++ src/Core/NWNCore.cs
@@ -1,40 +1,12 @@
-using System.Runtime.InteropServices;
-using System.Text;
-using NWN.Core.Native;
-
 namespace NWN.Core
 {
-  public static partial class NWNCore
+  public static class NWNCore
   {
-    /// <summary>
-    /// Gets or sets the encoding to use for native/managed string conversion.<br/>
-    /// Defaults to windows-1252.
-    /// </summary>
-    public static Encoding Encoding
-    {
-      get => NwStringMarshaller.Encoding;
-      set => NwStringMarshaller.Encoding = value;
-    }
-
     internal static ICoreFunctionHandler? FunctionHandler;
 
     public static void Init(ICoreFunctionHandler functionHandler)
     {
-      Marshal.PrelinkAll(typeof(NWNCore));
       FunctionHandler = functionHandler;
-    }
-
-    public static unsafe void Init(out CoreGameManager coreGameManager)
-    {
-      coreGameManager = new CoreGameManager();
-      Init(coreGameManager);
-
-      RegisterMainLoopHandler(&CoreGameManager.MainLoopHandler);
-      RegisterRunScriptHandler(&CoreGameManager.RunScriptHandler);
-      RegisterClosureHandler(&CoreGameManager.ClosureHandler);
-      RegisterSignalHandler(&CoreGameManager.SignalHandler);
-      RegisterAssertHandler(&CoreGameManager.AssertFailHandler);
-      RegisterCrashHandler(&CoreGameManager.CrashHandler);
     }
   }
 }
